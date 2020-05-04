796de36a6cddec816f818a955b7fdf048c130178
Maurice <m.lehmann94@gmail.com>
2020-05-04 16:19:47 +0200

    express.js installed, dockerfile created

---
+++ docker-images/express-image/src/index.js
@@ -0,0 +1,17 @@
+var Chance = require('chance');
+var chance = new Chance();
+var express = require('express');
+var app = express();
+
+//Avec GET / HTTP/1.0
+app.get('/', function(req, res){
+	res.send(chance.name());
+});
+
+app.get('/test', function(req, res){
+	res.send("Test is working! </br>");
+});
+
+app.listen(3333, function(){
+	console.log("Accepting HTTP req. on port 3333")
+})
